b7fb321afbe9b3e36c2d01b74602c4f169bef7b7
kuriyadibabu <kuriyadibabu@gmail.com>
2016-10-06 11:44:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element = document.getElementById('main_text');
 element.innerHTML = 'New Value';
 
 // Move the image
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onClick = function(){
     
     img.style.marginLeft = '100px';
