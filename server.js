05c90a1f47131772111662abe95ac491007e4f5d
kuriyadibabu <kuriyadibabu@gmail.com>
2016-10-06 15:58:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,11 +110,11 @@ app.get('/', function (req, res) {
 //  res.send(createTemplate(articles[articleName]));
    // res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 //});
-app.get('/article-Two', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
+//app.get('/article-Two', function (req, res) {
+// res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+//});
 //app.get('/article-Three', function (req, res) {
- // res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+// res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 //});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
