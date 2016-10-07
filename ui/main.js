d02a007d5a2c6903aadcc17adb3b06cad77b3564
kuriyadibabu <kuriyadibabu@gmail.com>
2016-10-07 11:01:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 // change the text of main_text div
-// var element = document.getElementById('main_text');
+ var element = document.getElementById('main_text');
 // element.innerHTML = 'New Value';
 
 // Move the image
