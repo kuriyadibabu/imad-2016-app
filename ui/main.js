983c8bbae61ef71bcda5aaa0cb0e37b4eda03da6
kuriyadibabu <kuriyadibabu@gmail.com>
2016-10-07 12:12:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,19 @@
+// Counter Code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+    
+    
+    // make a request to the counter end point
+    
+    // captue the response and store it in a variable
+    
+    // render the variable in the correct span
+    
+    counter = counter + 1;
+    var span = getElementById('count');
+    span.innerHTML = counter.toString();
+};
 
 
 
