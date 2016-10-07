f0f7b079b0f668bcb24a5476053e65be0120564b
kuriyadibabu <kuriyadibabu@gmail.com>
2016-10-07 11:58:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,22 @@
-console.log('Loaded!');
+
+
+
+
+// console.log('Loaded!');
 // change the text of main_text div
- var element = document.getElementById('main_text');
-element.innerHTML = 'New Value';
+// var element = document.getElementById('main_text');
+// element.innerHTML = 'New Value';
 
 // Move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-    
-    
-}
+// var img = document.getElementById('madi');
+// var marginLeft = 0;
+// function moveRight(){
+//    
+//     marginLeft = marginLeft + 1;
+//     img.style.marginLeft = marginLeft + 'px';
+// }
+// img.onclick = function () {
+//    var interval = setInterval(moveRight, 50);
+//     
+//     
+// }
